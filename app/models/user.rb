8cf3daed053219bf5b70a1a7cee4024971682325
hideto shiozawa <shiorange10592@yahoo.co.jp>
2016-08-06 11:09:52 +0000

    method add

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,12 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
+  
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items, through: :wants, source: :item
+  
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
 
 
   # 他のユーザーをフォローする
@@ -31,20 +37,32 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    have_items.find_or_create_by(item_id: item.id)    
   end
 
   def unhave(item)
+    have_item = have_items.find_by(item_id: item.id)  
+    have_item.destroy if have_item    
   end
 
   def have?(item)
+    have_users.include?(item)
   end
 
   def want(item)
+    wants.find_or_create_by(item_id: item.id)    
+    
   end
 
   def unwant(item)
+    
+    want_item = wants.find_by(item_id: item.id)  
+    want_item.destroy if want_item    
+
   end
 
   def want?(item)
+    want_users.include?(item)
+
   end
 end
---
+++ app/models/want.rb
@@ -0,0 +1,5 @@
+class Want < Ownership
+    
+    
+    
+end
